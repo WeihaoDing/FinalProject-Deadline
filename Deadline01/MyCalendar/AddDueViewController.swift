26c4830c6dc669dc3843dce85ab9c108e765c920
WeihaoDing <32556773+WeihaoDing@users.noreply.github.com>
2017-12-09 21:12:20 -0800

    addDue viewControllor change

--- Deadline01/MyCalendar/AddDueViewController.swift
+++ Deadline01/MyCalendar/AddDueViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class AddDueViewController: UIViewController {
 
+    @IBOutlet weak var subText: UITextField!
+    @IBOutlet weak var conText: UITextField!
+    @IBOutlet weak var datepicker: UIDatePicker!
+ 
+    var selectedDate : Date?
+    
+     /*
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX") // edited
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    let date = dateFormatter.date(from:myDate)!
+    dateFormatter.dateFormat = "dd/MM/yyyy"
+    let dateString = dateFormatter.string(from:date)
+    */
+    @IBAction func addDue(_ sender: UIButton) {
+        let subData : String = subText.text!
+        let conData : String = conText.text!
+        
+        
+        let due = Due(subject: subData, color: UIColor.black, content: conData, deadline: (Date().description), emergence: 10)
+        
+        let alert = UIAlertController(title: "confirmation", message: "You have added a Due successfully!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {
+            action in
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let nvc = storyBoard.instantiateViewController(withIdentifier: "mainCalendar")
+            self.present(nvc, animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.datepicker.addTarget(self, action: #selector(self.storeSelectedRow), for: UIControlEvents.valueChanged)
+       
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func storeSelectedRow(){
+        self.selectedDate = self.datepicker.date
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +58,7 @@ class AddDueViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+ 
     /*
     // MARK: - Navigation
 
